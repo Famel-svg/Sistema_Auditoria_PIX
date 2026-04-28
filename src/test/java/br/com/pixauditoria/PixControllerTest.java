0746b13707084fd5055f14188e06504cb2daf6bb
Famel.svg <rafaelandradetanimoto@gmail.com>
2026-04-28 00:12:50 -0300

    test(unit): atualiza PixControllerTest para suportar configuração de segurança
    - Importa SecurityConfig para desabilitar CSRF e permitir todas as requisições
    - Remove header de autorização no teste de validação

--- src/test/java/br/com/pixauditoria/PixControllerTest.java
+++ src/test/java/br/com/pixauditoria/PixControllerTest.java
@@ -1,11 +1,13 @@
 package br.com.pixauditoria;
 
+import br.com.pixauditoria.config.SecurityConfig;
 import br.com.pixauditoria.controller.PixController;
 import br.com.pixauditoria.service.PixService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PixController.class)
+@Import(SecurityConfig.class) // carrega o csrf().disable() e anyRequest().permitAll()
 class PixControllerTest {
 
     @Autowired private MockMvc mockMvc;
@@ -21,7 +24,6 @@ class PixControllerTest {
     @Test
     void deveRetornar400SeDadosInvalidos() throws Exception {
         mockMvc.perform(post("/api/pix/transferencias")
-                        .header("Authorization", "Basic dXNlcjpwYXNzd29yZA==")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"chaveDestino\":\"x\",\"valor\":-1}"))
                 .andExpect(status().isBadRequest());
